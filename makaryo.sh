aeed35423057df7e0fe8b4255725715d1da29297
pr4ka5a <kaka.prakasa@gmail.com>
2011-05-19 19:37:37 +0700

    perbaikan link pada makaryo.sh

--- makaryo.sh
+++ makaryo.sh
@@ -1,13 +1,13 @@
 
-pathjoel="~/"
+pathjoel="$HOME"
 echo "Masukkan nama projek:" 
 read nama
 echo Direktori yang akan dibuat adalah $nama , $nama/Lumbung , $nama/Surat , $nama/video/ , $nama/gambar/ . $nama/suara/
 if [ ! -d "$nama" ];then
-	mkdir -p $nama $nama/Lumbung $nama/Surat Videos/$nama-video Pictures/$nama-gambar Music/$nama-suara
-	ln -s $pathjoel/Videos/$nama-video $nama/video
-	ln -s $pathjoel/Pictures/$nama-gambar $nama/gambar
-	ln -s $pathjoel/Music/$nama-suara $nama/suara
+	mkdir -p $pathjoel/Documents/$nama $pathjoel/Documents/$nama/Lumbung $pathjoel/Documents/$nama/Surat $pathjoel/Videos/$nama-video $pathjoel/Pictures/$nama-gambar $pathjoel/Music/$nama-suara
+	ln -s $pathjoel/Videos/$nama-video $pathjoel/Documents/$nama/video
+	ln -s $pathjoel/Pictures/$nama-gambar $pathjoel/Documents/$nama/gambar
+	ln -s $pathjoel/Music/$nama-suara $pathjoel/Documents/$nama/suara
 	echo selamat !! direktori sukses dibuat..
 else
 	echo maaf, direktori sudah ada coba cek atau gunakan nama lain .. 
